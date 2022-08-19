f9a1966707baaf20acebeca4c1d092bff7b3cedf
Tazida <tazidarahman5@gmail.com>
2022-08-20 00:38:55 +0600

    done project

--- js/deposite.js
+++ js/deposite.js
@@ -13,9 +13,20 @@ document.getElementById("btn-deposite").addEventListener("click", function () {
   const previousdepositeTotalString = depositeTotalElement.innerText;
   const previousdepositeTotal = parseFloat(previousdepositeTotalString);
 
+  // step 4: add numbers to set the total deposite
   const currentDepositeTotal = previousdepositeTotal + newdepositeAmount;
-
+  // set the deposite total
   depositeTotalElement.innerText = currentDepositeTotal;
+
+  // step 5: get balance current total
+  const balanceTotalElement = document.getElementById("balance-total");
+  const previousBalanceString = balanceTotalElement.innerText;
+  const previousBalanceTotal = parseFloat(previousBalanceString);
+
+  // step 6: calculate current total balance
+  const currentBalanceTotal = previousBalanceTotal + newdepositeAmount;
+  // set the balance total
+  balanceTotalElement.innerText = currentBalanceTotal;
 
   // step 7: clear the deposite field
   depositeField.value = "";
